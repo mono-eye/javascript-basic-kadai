80e05b19edeea9bcea39965d6bf8c097e7a06989
mono-eye <161995888+mono-eye@users.noreply.github.com>
2024-04-02 19:13:31 +0900

    kadai_005　２回目

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+/*================================
+ bmi.js
+================================*/
+
+// 変数の宣言
+bodyWeight = 68;
+height = 1.7;
+
+// 定数の値の代入bmiが～～計算
+const bmi = (bodyWeight / (height * height));
+
+// コンソールへの出力するのはbmi
+console.log(bmi);
